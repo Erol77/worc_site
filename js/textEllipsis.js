822bd112acdc3c6cfe4d4b1c3ec3354c589dd194
Erol <veliev77@mail.ru>
2022-06-14 12:04:41 +0300

    text decoration no add

--- js/textEllipsis.js
+++ js/textEllipsis.js
@@ -5,30 +5,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function addTextDecoration(selector) {
         const text = Array.from(document.querySelectorAll(selector));
+        
         text.forEach(i => {
-            if (overflow(i.parentElement)) {
-                console.log('wrong ' + i.textContent)
-                i.parentElement.classList.add('hide__text');
-                i.parentElement.setAttribute('data-show','text')
-            } else {
-                if (i.classList.contains('hide__text')) { i.parentElement.classList.remove('hide__text') 
-                i.parentElement.removeAttribute('data-show')
-            }
-                // console.log('delete ty class its okkey ' + i)
-            }
+        //     if (overflow(i.parentElement)) {
+        //         console.log('wrong ' + i.textContent)
+        //         i.parentElement.classList.add('hide__text');
+        //         i.parentElement.setAttribute('data-show','text');
+        //         var el = document.createElement('div');
+        // el.classList.add('tooltip');
+        // el.setAttribute('data-text',i.textContent)
+               
+        //         // insertBefore(newNode, referenceNode.nextSibling);
+        //         i.append(el);
+        //     } else {
+        //         if (i.classList.contains('hide__text')) { i.parentElement.classList.remove('hide__text') 
+        //         i.parentElement.removeAttribute('data-show')
+        //     }
+        //         // console.log('delete ty class its okkey ' + i)
+        //     }
+        if (i.scrollWidth > i.offsetWidth) {
+            console.log('wrong ' + i.textContent)
+            i.classList.add('hide__text');
+            i.setAttribute('data-show','text');
+    //         var el = document.createElement('div');
+    // el.classList.add('tooltip');
+    // el.setAttribute('data-text',i.textContent)
+           
+            // insertBefore(newNode, referenceNode.nextSibling);
+            // i.append(el);
+        } else {
+            if (i.classList.contains('hide__text')) { i.classList.remove('hide__text') 
+            i.removeAttribute('data-show')
+        }
+            // console.log('delete ty class its okkey ' + i)
+        }
         });
 
     }
     // addTextDecoration('.select__toggle span');
-    addTextDecoration('.table_fio span');
-    addTextDecoration('.form-control option');
-    addTextDecoration('.table_body_item-type span');
-    addTextDecoration('.table_phone span');
+    addTextDecoration('.table_fio');
+    // addTextDecoration('.form-control option');
+    addTextDecoration('.table_body_item-type');
+    addTextDecoration('.table_phone');
 
     
-    document.addEventListener('click', () => {
+    //document.addEventListener('click', () => {
         // addTextDecoration('.select__toggle');
-        addTextDecoration('.form-control');
-    })
+        // addTextDecoration('.form-control');
+   // })
 
 })
