3d63f92d32b4c9496137c84ab3517c6a5fd235d6
Erol <veliev77@mail.ru>
2022-06-15 09:27:18 +0300

    fixed textEltip

--- js/textEllipsis.js
+++ js/textEllipsis.js
@@ -6,19 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     function addTextDecoration(selector) {
         const text = Array.from(document.querySelectorAll(selector));
 
-        text.forEach(i => {
+        text.map(i => {
             if (i.scrollWidth > i.offsetWidth) {
                 i.classList.add('hide__text');
                 i.setAttribute('data-show', 'text');
- i.setAttribute('onmouseover', `toolTip('${i.textContent.trim()}')`);
-i.setAttribute('onmouseout', `toolTip()`);
-                //         var el = document.createElement('div'); data-tooltip-mouseover=
-                // el.classList.add('tooltip');
-                // el.setAttribute('data-text',i.textContent)
-
-                // insertBefore(newNode, referenceNode.nextSibling);
-                // i.append(el);
-
+                i.setAttribute('onmouseover', `toolTip('${i.textContent.trim()}')`);
+                i.setAttribute('onmouseout', `toolTip()`);
             } else {
                 if (i.classList.contains('hide__text')) {
                     i.classList.remove('hide__text')
@@ -26,7 +19,6 @@ i.setAttribute('onmouseout', `toolTip()`);
                     i.removeAttribute('onmouseover');
                     i.removeAttribute('onmouseout');
                 }
-                // console.log('delete ty class its okkey ' + i)
             }
         });
 
@@ -35,52 +27,36 @@ i.setAttribute('onmouseout', `toolTip()`);
     function addTextDecorationSelect(selector) {
         const text = Array.from(document.querySelectorAll(selector));
 
-        text.forEach(i => {
+        text.map(i => {
             if (i.scrollWidth > i.offsetWidth) {
                 i.classList.add('hide__text');
-                // i.parentElement.parentElement.classList.add('hide__text');
-                // i.parentElement.parentElement.setAttribute('data-show', 'text');
- i.parentElement.setAttribute('onmouseover', `toolTip('${i.textContent.trim()}')`);
- i.parentElement.setAttribute('onmouseout', `toolTip()`);
-                //         var el = document.createElement('div');
-                // el.classList.add('tooltip');
-                // i.parentElement.parentElement.setAttribute('data-text', i.textContent)
-
-                //         // insertBefore(newNode, referenceNode.nextSibling);
-                //         i.append(el);
+                i.parentElement.setAttribute('onmouseover', `toolTip('${i.textContent.trim()}')`);
+                i.parentElement.setAttribute('onmouseout', `toolTip()`);
             } else {
                 if (i.classList.contains('hide__text')) {
-                    // i.parentElement.classList.remove('hide__text');
                     i.classList.remove('hide__text');
-                    // i.parentElement.removeAttribute('data-show')
-                   i.parentElement.removeAttribute('onmouseover');
-                //    i.parentElement.removeAttribute('onmouseout');
+                    i.parentElement.removeAttribute('onmouseover');
                 }
-                //         // console.log('delete ty class its okkey ' + i)
             }
         })
     };
 
-
-    // addTextDecoration('.select__toggle');
     addTextDecoration('.table_fio');
-    // addTextDecoration('.form-control option');
     addTextDecoration('.table_body_item-type');
     addTextDecoration('.table_body_item a.table_phone');
-// const tooltip = document.getElementById('tooltip');
-/*
-    document.addEventListener('mousemove', (e) => {
-console.log(e.offsetX, '   ' + e.offsetY + '   ' + e.target.getAttribute('data-text'));
-// if (e.target.hasAttribute('data-show')){
-if (e.target.classList.contains('.table_fio')) {
-       tooltip.classList.toggle('hide');
-tooltip.innerHTML ='';
-tooltip.innerHTML = e.target.getAttribute('data-text') + '<div class="tooltip-arrow toptobottom center "></div>';
-        // addTextDecoration('.form-control'); .table_fio
-}else{
-setTimeout(tooltip.classList.toggle('hide'),1000)
-}
-    })*/
+    /*
+        document.addEventListener('mousemove', (e) => {
+    console.log(e.offsetX, '   ' + e.offsetY + '   ' + e.target.getAttribute('data-text'));
+    // if (e.target.hasAttribute('data-show')){
+    if (e.target.classList.contains('.table_fio')) {
+           tooltip.classList.toggle('hide');
+    tooltip.innerHTML ='';
+    tooltip.innerHTML = e.target.getAttribute('data-text') + '<div class="tooltip-arrow toptobottom center "></div>';
+            // addTextDecoration('.form-control'); .table_fio
+    }else{
+    setTimeout(tooltip.classList.toggle('hide'),1000)
+    }
+        })*/
 
     document.addEventListener('click', () => {
         addTextDecorationSelect('.select__toggle');
@@ -88,14 +64,14 @@ setTimeout(tooltip.classList.toggle('hide'),1000)
     })
 })
 function getCoords(elem) {
-    let box = elem.getBoundingClientRect();  
+    let box = elem.getBoundingClientRect();
     return {
-      top: box.top + window.pageYOffset,
-      right: box.right + window.pageXOffset,
-      bottom: box.bottom + window.pageYOffset,
-      left: box.left + window.pageXOffset
+        top: box.top + window.pageYOffset,
+        right: box.right + window.pageXOffset,
+        bottom: box.bottom + window.pageYOffset,
+        left: box.left + window.pageXOffset
     };
-  }
+}
 
 document.onmousemove = moveTip;
 function moveTip(e) {
@@ -110,7 +86,7 @@ function moveTip(e) {
     }
     // Показывать слой справа от курсора 
     if ((x + w + 10) < document.body.clientWidth) {
-        floatTipStyle.left =   x -50 + 'px';        // Показывать слой слева от курсора
+        floatTipStyle.left = x - 50 + 'px';        // Показывать слой слева от курсора
         floatTipStyle.arrow = 'left';
     } else {
         floatTipStyle.left = x - w + 'px';
@@ -120,14 +96,14 @@ function moveTip(e) {
     // center= c.left+c.right/c.left;
     // floatTipStyle.left = center+'px';
     // console.log(c.top+'top  '+center);
-    floatTipStyle.top = y -80 + 'px';
+    floatTipStyle.top = y - 80 + 'px';
 }
 
 function toolTip(msg) {
     floatTipStyle = document.getElementById("tooltip").style;
     if (msg) {
-          document.getElementById("tooltip").innerHTML = msg + '<div class="tooltip-arrow toptobottom '+floatTipStyle.arrow+' "></div>';
-        floatTipStyle.display = "block"; 
+        document.getElementById("tooltip").innerHTML = msg + '<div class="tooltip-arrow toptobottom ' + floatTipStyle.arrow + ' "></div>';
+        floatTipStyle.display = "block";
     } else {
         floatTipStyle.display = "none"; //  floatTip
     }
